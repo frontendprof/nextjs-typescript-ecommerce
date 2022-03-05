4edea0490c6c41b44c410742700e0da8e6489d2a
frontendprof <abdumalksharipov@gmail.com>
2022-03-05 23:34:12 +0500

    Container wrapper component && style

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 import Link from 'next/link';
+
+import { Container } from '@components/ui';
+
 import s from './Hero.module.css';
 
 interface Props {
@@ -9,15 +12,17 @@ interface Props {
 const Hero: FC<Props> = ({ headline, description }) => {
   return (
     <div style={{ background: 'black' }}>
-      <div className={s.root}>
-        <h2 className={s.headline}>{headline}</h2>
-        <div>
-          <p className={s.description}>{description}</p>
-          <Link href="/">
-            <a className={s.link}>Read More</a>
-          </Link>
+      <Container el="div">
+        <div className={s.root}>
+          <h2 className={s.headline}>{headline}</h2>
+          <div className="flex-1 max-w-4xl">
+            <p className={s.description}>{description}</p>
+            <Link href="/">
+              <a className={s.link}>Read More</a>
+            </Link>
+          </div>
         </div>
-      </div>
+      </Container>
     </div>
   );
 };
